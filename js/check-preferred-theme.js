201e3eb4bc84b8bdc6976c20ec27fd33a7f1c744
HisokaBluee <mattias.gervilliers@etu.univ-amu.fr>
2021-11-21 19:09:50 +0100

    cookie

--- js/check-preferred-theme.js
+++ js/check-preferred-theme.js
@@ -1,3 +1,6 @@
 if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
     document.getElementById("dark-theme").disabled=false;
 }
+if (cookies.get("style") == "light") {
+    document.getElementById("dark-theme").disabled=true;
+}
--- js/switch-theme.js
+++ js/switch-theme.js
@@ -1,9 +1,12 @@
+
 function switchTheme() {
         sheet = document.getElementById("dark-theme");
         if (sheet.disabled) {
+                document.cookie = "style=dark";
                 sheet.disabled=false;
         }
         else {
+                document.cookie = "style=light";
                 sheet.disabled=true;
         }
 }
